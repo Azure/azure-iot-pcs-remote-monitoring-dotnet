fabc1e680cd3949bee2072b92f00658fd1246d16
Devis Lucato <devis@lucato.it>
2017-09-16 21:13:29 -0700

    Add Auth submodule

--- scripts/update-gitsubmodules.sh
+++ scripts/update-gitsubmodules.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
 APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/"
 
 cd $APP_HOME
 
 git checkout master
+git pull
 
-echo "### CLI"
+COL="\e[93m\e[1m"
+NOCOL="\e[0m"
+
+echo -e "${COL}### Auth${NOCOL}"
+git submodule update auth              && cd auth              && git checkout master && git pull && cd ..
+echo -e "${COL}### CLI${NOCOL}"
 git submodule update cli               && cd cli               && git checkout master && git pull && cd ..
-echo "### Config"
+echo -e "${COL}### Config${NOCOL}"
 git submodule update config            && cd config            && git checkout master && git pull && cd ..
-echo "### Device Simulation"
+echo -e "${COL}### Device Simulation${NOCOL}"
 git submodule update device-simulation && cd device-simulation && git checkout master && git pull && cd ..
-echo "### IoT Hub Manager"
+echo -e "${COL}### IoT Hub Manager${NOCOL}"
 git submodule update iothub-manager    && cd iothub-manager    && git checkout master && git pull && cd ..
-echo "### Storage Adapter"
+echo -e "${COL}### Storage Adapter${NOCOL}"
 git submodule update storage-adapter   && cd storage-adapter   && git checkout master && git pull && cd ..
-echo "### Telemetry"
+echo -e "${COL}### Telemetry${NOCOL}"
 git submodule update telemetry         && cd telemetry         && git checkout master && git pull && cd ..
-echo "### Telemetry Agent"
+echo -e "${COL}### Telemetry Agent${NOCOL}"
 git submodule update telemetry-agent   && cd telemetry-agent   && git checkout master && git pull && cd ..
-echo "### Web UI"
+echo -e "${COL}### Web UI${NOCOL}"
 git submodule update webui             && cd webui             && git checkout master && git pull && cd ..
