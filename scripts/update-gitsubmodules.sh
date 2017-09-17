088c4ceef23e3cb606f33a65fbbc6360516a93fa
Devis Lucato <devis@lucato.it>
2017-09-16 21:01:24 -0700

    Update submodules

--- scripts/update-gitsubmodules.sh
+++ scripts/update-gitsubmodules.sh
@@ -6,11 +6,19 @@ cd $APP_HOME
 
 git checkout master
 
+echo "### CLI"
 git submodule update cli               && cd cli               && git checkout master && git pull && cd ..
+echo "### Config"
+git submodule update config            && cd config            && git checkout master && git pull && cd ..
+echo "### Device Simulation"
 git submodule update device-simulation && cd device-simulation && git checkout master && git pull && cd ..
+echo "### IoT Hub Manager"
 git submodule update iothub-manager    && cd iothub-manager    && git checkout master && git pull && cd ..
+echo "### Storage Adapter"
 git submodule update storage-adapter   && cd storage-adapter   && git checkout master && git pull && cd ..
+echo "### Telemetry"
 git submodule update telemetry         && cd telemetry         && git checkout master && git pull && cd ..
+echo "### Telemetry Agent"
 git submodule update telemetry-agent   && cd telemetry-agent   && git checkout master && git pull && cd ..
-git submodule update ui-config         && cd ui-config         && git checkout master && git pull && cd ..
+echo "### Web UI"
 git submodule update webui             && cd webui             && git checkout master && git pull && cd ..
